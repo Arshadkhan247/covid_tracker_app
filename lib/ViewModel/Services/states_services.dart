513c810fa63ab26d3bf3fd1f5e9a31d89d6cd515
Arshadkhan247 <ak4322866@gmail.com>
2023-06-10 06:34:40 +0500

    fetch data for country list in services file.

--- lib/ViewModel/Services/states_services.dart
+++ lib/ViewModel/Services/states_services.dart
@@ -5,7 +5,6 @@ import 'package:covid_tracker_app/ViewModel/Utilities/app_url.dart';
 import 'package:http/http.dart' as http;
 
 class StatesServices {
-
 // calling Api data ...
 
   Future<WorldStatesModel> fetchWorldStatesData() async {
@@ -15,6 +14,18 @@ class StatesServices {
       var data = jsonDecode(response.body.toString());
 
       return WorldStatesModel.fromJson(data);
+    } else {
+      throw Exception('Error Occured');
+    }
+  }
+
+  Future<List<dynamic>> countriesListApi() async {
+    dynamic data;
+    final response = await http.get(Uri.parse(AppUrl.countriesList));
+
+    if (response.statusCode == 200) {
+      data = jsonDecode(response.body.toString());
+      return data;
     } else {
       throw Exception('Error Occured');
     }
